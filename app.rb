7dcfa3bfa2402b4f0fc786a595d26c2f8be3d6ec
Seth Hochberg <seth@sethhochberg.com>
2015-04-19 22:41:02 -0400

    use ses gem

--- app.rb
+++ app.rb
@@ -2,32 +2,59 @@ require 'sinatra'
 require 'sinatra/config_file'
 require 'sinatra/json'
 require 'json'
-require 'pony'
+require 'aws/ses'
 
 config_file 'config.yml'
 
+class ParamsMissingError
+end
+
+class MailSendError
+end
+
+get '/' do
+	status 200
+	text 'nothing to see here, move along'
+end
+
 get '/ping' do
+	status 200
 	json pong: true, time: Time.now.to_s
 end
 
 post '/mail' do
 	destination = params[:destination]
+	name 				= params[:name]
+	message 		= params[:message]
+	sender      = params[:sender]
 
-	Pony.mail(
-	{
-		:from => params[:sender],
-	    :to => destination,
-	    :subject => 'Request From: ' + params[:name],
-	    :body => params[:body],
-	    :via => :smtp,
-	    :via_options => {
-	     :address              => settings.smtp.address,
-	     :port                 => settings.smtp.port,
-	     :enable_starttls_auto => settings.smtp.starttls_auto,
-	     :user_name            => settings.smtp.user,
-	     :password             => settings.smtp.password,
-	     :authentication       => settings.smtp.auth_type, 
-	     :domain               => settings.smtp.domain 
-		}
-	})	
+	unless destination && name && message && sender
+		raise ParamsMissingError
+	end
+
+	ses = AWS::SES::Base.new(
+	  :access_key_id     => settings.ses['key'], 
+	  :secret_access_key => settings.ses['token'],
+	  :server => settings.ses['server']
+	)
+
+	ses.send_email(
+    :to        => Array.wrap(destination)
+    :source    => sender,
+    :subject   => settings.subject_prepend + ' : ' + name,
+    :text_body => name + ' : ' + message
+	)
+
+	status 200
+	json message: settings.success_message
+end
+
+error ParamsMissingError do
+	status 400
+	json error: settings.unprocessable_message
+end
+
+error MailSendError do
+	status 503
+	json error: settings.fail_message
 end
